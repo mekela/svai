8fe11ae53b254c1456b8f021077a527297559739
alex <sania_free@i.ua>
2016-06-02 13:59:00 +0300

    modals

--- js/common.js
+++ js/common.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
 	//fancybox
 	$('.fancybox').fancybox({
+		closeBtn: false,
 		helpers: {
+			overlay: {
+				locsked: false
+			}
+		}
+	});
+
+	$('.fancybox-gallery').fancybox({
+		arrows: true,
+		helpers: {
+			title : {
+            	type : 'float'
+        	},
 			overlay: {
 				locked: false
 			}
@@ -11,4 +24,11 @@ $(document).ready(function() {
 	//bxslider
 	$('.reviews_slider ul').bxSlider({
 	});
+
+	 //scroll anchor
+    $('.main_menu li a, .arrow_top').on('click',function(){
+		$('html,body').animate({scrollTop:$($(this).attr('href')).offset().top-0},800);
+		return false;
+	});
 });
+
